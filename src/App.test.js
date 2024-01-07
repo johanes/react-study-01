ee430b2e160775710c03bb609bb7688d6327cedf
Johanes Strahl <dev@johanes.com.br>
2024-01-07 17:19:37 +0100

    removed unused files and test update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Aligenigenas title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Aligenigenas/i);
   expect(linkElement).toBeInTheDocument();
 });
